b3b0ada5f9485901b8bd433227f5930c273a0628
Emanuel Suriano <emanuel.suriano@revolut.com>
2023-02-27 10:57:37 +0100

    Add vercel integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,10 @@
 import { defineConfig } from 'astro/config';
-
-// https://astro.build/config
 import react from '@astrojs/react';
+import vercel from '@astrojs/vercel/serverless';
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [react()],
   output: 'server',
+  adapter: vercel(),
 });
